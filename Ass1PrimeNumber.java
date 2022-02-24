fbe3b04da6bab733d28b983a61d1cdbea1969338
RekhaMunusamy <100158399+RekhaMunusamy@users.noreply.github.com>
2022-02-24 23:48:07 +0530

    Update Ass1PrimeNumber.java

--- Ass1PrimeNumber.java
+++ Ass1PrimeNumber.java
@@ -3,10 +3,10 @@ package week1.day2;
 public class Ass1PrimeNumber {
 
 	public static void main(String[] args) {
-		int n = 12;
+		int n = 13;
 		boolean prime=true;
 		for (int i = 2; i < n ; i++) {
-			if (n % 2 == 0)
+			if (n % i == 0)
 				prime=false;
 			break;
 			}  
